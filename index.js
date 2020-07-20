6d37141ec817727848bac5aafbb2641dbb9a3657
Nuha Al-Sameai <noha.pepeep1@gmail.com>
2020-07-20 01:55:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,14 +2,15 @@
 
 
 let n="surprise";
-let array;
+let arr="";
 function writeCards(array,n) {
+  
   for (let i = 0; i < array.length; i++) {
     var l='Thank you, '+array[i]+', for the wonderful '+n+' gift!';
     console.log(l);
-    
+    arr+=l;
   }
- return array;
+ return arr;
 }
 
 function countDown(n){
