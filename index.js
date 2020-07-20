5fd3169e3ab849cce46ab178a4eca5cdb026de78
Nuha Al-Sameai <noha.pepeep1@gmail.com>
2020-07-20 01:49:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solutions in this file
+
+
+let n="surprise";
+let array;
+function writeCards(array,n) {
+  for (let i = 0; i < array.length; i++) {
+    var l='Thank you, '+array[i]+', for the wonderful '+n+' gift!';
+    console.log(l);
+    
+  }
+ return array;
+}
+
+function countDown(n){
+  var i=n;
+  while(i>=0){
+    console.log(i);
+    i--;
+   
+  }
+}
+writeCards(["Ada", "Brendan", "Ali"],"birthday");
+countDown(10);
+console.log(array[0]);
+console.log(array[1]);
+console.log(array[2]);
+
